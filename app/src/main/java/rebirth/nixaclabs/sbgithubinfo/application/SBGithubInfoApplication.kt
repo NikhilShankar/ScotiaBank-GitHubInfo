92bc21662e8b37e74d0ec65c5c85a7ec287eeaa6
nixacfilms <nikhil.nixaclabs@gmail.com>
2026-01-22 16:01:20 -0500

    Added application class for Hilt and added internet permission in manifest

---
+++ app/src/main/java/rebirth/nixaclabs/sbgithubinfo/application/SBGithubInfoApplication.kt
@@ -0,0 +1,13 @@
+package rebirth.nixaclabs.sbgithubinfo.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class SBGithubInfoApplication: Application() {
+
+
+
+
+}
