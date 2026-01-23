e28d2f94d5f661686b644f1d6a4f7c2427072290
nixacfilms <nikhil.nixaclabs@gmail.com>
2026-01-23 15:37:22 -0500

    Added documentation of how MockDataSource can be helpful and its usage using named qualifiers

--- app/src/main/java/rebirth/nixaclabs/sbgithubinfo/data/source/local/MockGithubDataSource.kt
+++ app/src/main/java/rebirth/nixaclabs/sbgithubinfo/data/source/local/MockGithubDataSource.kt
@@ -1,4 +1,9 @@
 package rebirth.nixaclabs.sbgithubinfo.data.source.local
 
+
+/**
+ * This mock data source can be used to test the UI offline wherein remote data source is not available.
+ * Named qualifiers to replace the remote data source with this one is already added in di package.
+ */
 class MockGithubDataSource {
 }
